0693f2d73493358f866c72bdd655b561ddecb52a
Ablai2 <erbulanablai@gmail.com>
2024-08-09 11:25:58 +0800

    update 8

---
+++ java.js
@@ -0,0 +1,65 @@
+// console.log("Hello", "string" )
+// console.log(true, "boolean" )
+// console.log("Name", "sting" )
+// console.log(33, "number" )
+// console.log("90", "string"  )
+// console.log(90+"77",  "string")
+// console.log( {name:'Anu', age:15}, "object" )
+// console.log(false, "boolean" )
+// console.log(1655/5,"number" )
+// console.log(["javascript", "html", "css"] , "array" )
+
+
+
+// let a = '50'
+
+// let b = '300'
+
+// let temp;
+
+// temp= a;
+
+// a = b;
+
+// b = temp;
+
+
+
+
+// let ner = 'ablai'
+// let age = 13;
+
+// console.log("Намайг " + ner + " гэдэг. Би " + age + " настай.")
+
+
+// let  futureYear = 2040;
+
+// let bornYear = 1999;
+
+// futureYear -  bornYear
+// 41
+
+// let time=2;
+// let day=2;
+// let toTalTime= day * 24+time
+// console.log(toTalTime);
+
+
+// const year=2024;
+// let bornyear=1999;
+// let bornmonth=10;
+// let bornday=20;
+// let buh =( year - bornyear ) * 365 + bornmonth * 30 + bornday
+// console.log(buh);
+
+
+
+
+
+// let a
+// let b=1234%1000;
+let c=
+a = parseInt(1234)
+
+
+
